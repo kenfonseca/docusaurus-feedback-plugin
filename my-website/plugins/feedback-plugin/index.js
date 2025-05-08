a79cec943fb1a93498c0c133bc547a3c50524e91
Kenneth <tuo64805@temple.edu>
2025-05-08 14:43:12 -0400

    Added custom CLI command

--- my-website/plugins/feedback-plugin/index.js
+++ my-website/plugins/feedback-plugin/index.js
@@ -14,6 +14,18 @@ module.exports = function (context, options){
                     }
                 }]
             };
+        },
+
+        // extendCli takes in a cli object (command.js type object)
+        extendCli(cli){
+            // To run this command in terminal: 'npm run docusaurus feedback-plugin'
+            cli.command('feedback-plugin')
+            // idek
+            .description('this is a custom command')
+            // what executes when the command is called
+            .action(() => {
+                console.log('Hello from the plugin');
+            })
         }
     };
 }
